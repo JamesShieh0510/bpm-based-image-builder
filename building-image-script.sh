3d138bb241eaed302ae2e1ea3d6a4ff1a6c9a812
James Shieh <jamesshieh0510@gmail.com>
2018-06-10 21:45:36 +0800

    add commet

--- building-image-script.sh
+++ building-image-script.sh
@@ -1,6 +1,8 @@
-#docker login -u autolab -p auto63906
-WORKPATH=$(pwd)
+#登入 image repository 以便將開發完成的image上傳
 docker login
+
+#取得BPM的application source code
+WORKPATH=$(pwd)
 rm -R ./application-source-code
 mkdir ./application-source-code
 cd    ./application-source-code
@@ -8,11 +10,20 @@ git clone https://github.com/JamesShieh0510/bpm.git
  chmod -R 777 ./bpm
 cd $WORKPATH
 
-version=0.0.1
-docker build -t autolab/bpm:$version ./
-#docker run -it autolab/bpm:$version
-docker run -p 8089:8054 autolab/bpm:$version
-#sudo docker commit -m "autolab bpm container app" $image_id autolab/bpm:0.0.1
-#docker images bpm*
 
+#建立image
+version=1.0.0
+docker build -t autolab/bpm:$version ./
+
+
+#將image上傳到image repository上
+docker commit -m "BPM Containerized App, Autolab, NCKU" $image_id autolab/bpm:$version
+IMAGE_REPOSITORY_USER=autolab
+IMAGE_NAME=bpm:$version
+docker tag autolab/bpm:$version $IMAGE_REPOSITORY_USER/$IMAGE_NAME
+docker images autolab/*
+docker push $IMAGE_REPOSITORY_USER/$IMAGE_NAME
+
+#執行container化的bpm
+docker run -p 8089:8054 autolab/bpm:$version
 #. ./building--image-script.sh
